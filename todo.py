85c8e40adbf0ddfd51c00cc753d3738874d5e0c2
tbarton782 <tbarton782@gmail.com>
2021-02-06 19:44:19 -0800

    initial commit

---
+++ todo.py
@@ -0,0 +1,9 @@
+"""
+
+1. Optimize feature matching
+2. Add filters to reduce the algorithm workload (ex: edges, colors, texture, shape)
+3. Incorporate image cropping
+4. Incorporate IPWebcam
+5. Incorporate dialog (ex: "is this your rock?", etc)
+
+"""
